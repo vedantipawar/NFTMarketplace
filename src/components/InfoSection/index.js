c6e8211e6b637fb1d6c3c367c073b1de0c02ebe9
Vedanti Pawar <vedantipawar0801@gmail.com>
2024-01-25 21:42:41 -0800

    Info Section

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './infoSection.css';
+import { INFO_ITEMS } from '../../data/infoItems';
+import InfoItem from './InfoItem';
 
 const InfoSection = () => {
   return (
-    <div>InfoSection</div>
+    <div className="info-section">
+      <div className="is-heading absolute-center">
+        <span className="heading-gradient">Create and Sell you NFTs</span>
+      </div>
+      <div className="is-items-container">
+        {INFO_ITEMS.map((_infoItem)=> (
+          <InfoItem item= {_infoItem} />
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/data/infoItems.js
@@ -0,0 +1,20 @@
+export const INFO_ITEMS = [
+    {
+        id: 1,
+        icon: require('../assets/icons/wallet.png'),
+        section_title: "Set up your Wallet",
+        description: "Set up wallet of your choice"
+    },
+    {
+        id: 2,
+        icon: require('../assets/icons/upload.png'),
+        section_title: "Create and upload your creation",
+        description: "Set up wallet of your choice"
+    },
+    {
+        id: 1,
+        icon: require('../assets/icons/forSale.png'),
+        section_title: "List them For sale",
+        description: "Set up wallet of your choice"
+    }
+]
